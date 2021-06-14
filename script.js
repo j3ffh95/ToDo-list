2fec8f89e4841596efe9021f430cbd1853fc1f4d
Jefferson Huisa <j3ffh95@gmail.com>
2021-06-14 11:03:10 -0400

    Iterate trough todosEl array

--- script.js
+++ script.js
@@ -53,4 +53,11 @@ function updateLS() {
   todosEl = document.querySelectorAll("li");
 
   const todos = [];
+
+  todosEl.forEach(todoEl => {
+    todos.push({
+      text: todoEl.innerText,
+      completed: todoEl.classList.contains("completed"),
+    });
+  });
 }
