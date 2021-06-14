772520c24871f82a16573e51aa6505cc9840ea24
Jefferson Huisa <j3ffh95@gmail.com>
2021-06-14 11:00:01 -0400

    Added an if logic

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ const input = document.getElementById("input");
 const todosUL = document.getElementById("todos");
 
 const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+  todos.forEach(todo => addTodo(todo));
+}
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+
+  addTodo();
+});
